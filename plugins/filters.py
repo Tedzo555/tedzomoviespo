c22ebd52b33849c507b4aa0281f3ee2e6cfa8d5b
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-07-06 22:07:16 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,5 +1,5 @@
 import io
-from pyrogram import filters, Client
+from pyrogram import filters,idle, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
