34c829a00505294f820092eb2c8bec8c2329b403
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-08-02 20:33:18 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-client = AsyncIOMotorClient(mongodb+srv://thetedzo:thetedzo@cluster0.hbw7w.mongodb.net/?retryWrites=true&w=majority)
+client = AsyncIOMotorClient("mongodb+srv://thetedzo:thetedzo@cluster0.hbw7w.mongodb.net/?retryWrites=true&w=majority")
 db = client[DATABASE_NAME]
 instance = Instance.from_db(db)
 
