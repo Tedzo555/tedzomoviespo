fa6f3687e0bdd32c8d94cc6907bce70d6b4ee3d6
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-08-02 20:20:53 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,12 +1,12 @@
 import pymongo
 
-from info import DATABASE_URI, DATABASE_NAME
+from info import D, DATABASE_NAME
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI)
+myclient = pymongo.MongoClient(mongodb+srv://thetedzo:thetedzo@cluster0.hbw7w.mongodb.net/?retryWrites=true&w=majority)
 mydb = myclient[DATABASE_NAME]
 mycol = mydb['CONNECTION']   
 
